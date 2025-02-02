9ee10d12f5023c7b87b557e3c0c22b0d2fe6bbab
Dhruvpat12 <95866395+Dhruvpat12@users.noreply.github.com>
2025-02-02 17:54:26 +0530

    Update 2025_01_31_171337_create_categires_table.php

--- database/migrations/2025_01_31_171337_create_categires_table.php
+++ database/migrations/2025_01_31_171337_create_categires_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->foreignId('parent_id')->nullable()->constrained('categories')->onDelete('cascade');
+           $table->string('category_image');
             $table->timestamps();
         });
     }
